012c29eead837463c32372c0395271b21497f6c7
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2022-09-28 12:49:38 -0400

    fix import

--- utilities/coinbase.py
+++ utilities/coinbase.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from abc import abc
-from abc import abstractmethod
 from dataclasses import dataclass
 from dataclasses import field
 from datetime import datetime
