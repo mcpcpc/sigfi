e000ff270548273748fba40a1f26946a71675b16
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2022-09-28 09:57:21 -0400

    change port 8080 to 80

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,7 +5,7 @@ import multiprocessing
 import os
 
 chdir = '/root'
-bind = ':8080'
+bind = ':80'
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = multiprocessing.cpu_count() * 2 + 1
 worker_connections = 1000 * workers
