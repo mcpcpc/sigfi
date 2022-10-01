5ec6f92c97eb805cea299e33a0b86c90860559bb
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2022-09-30 20:16:12 -0400

    change seld.period to period

--- utilities/options.py
+++ utilities/options.py
@@ -115,7 +115,7 @@ class CandlesFigures:
             ma.append(ma_)
         self.figure.add_trace(
             graph_objects.scatter(
-                name="MA" + seld.period,
+                name="MA" + period,
                 x=self.timestamp,
                 y=ma,
                 line_color=dmc.theme.DEFAULT_COLORS["yellow"],
